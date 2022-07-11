69c3f5cfcaf1c16038fdb228e765e21d66ad5110
Irham F Rufdo <irham.f.rufdo@gmail.com>
2022-07-11 13:19:30 +0700

    update Dumbways/Day 4 - Creating Blog Page

--- assets/js/myblog.js
+++ assets/js/myblog.js
@@ -1,20 +1,5 @@
 let myblogs = []
 
-let month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
-
 function addBlog(event) {
     event.preventDefault()
 
@@ -28,7 +13,6 @@ function addBlog(event) {
         title,
         content,
         image,
-        postedAt: new Date()
     }
 
     myblogs.push(myblog)
@@ -56,7 +40,7 @@ function rendermyblogs() {
                     <a href="myblog-detail.html" target="_blank">${myblogs[i].title}</a>
                 </h1>
                 <div class="detail-blog-content">
-                    ${getFullTime(myblogs[i].postedAt)} | Irham Fatriyandas Rufdo
+                10 Jul 2022 10:30 WIB | Irham Fatriyandas Rufdo
                 </div>
                 <p>
                     ${myblogs[i].content}
@@ -66,51 +50,10 @@ function rendermyblogs() {
                 <br>
                 <div style="text-align: right;
                 margin: 20px">
-                    <span style="font-size: 15px; color: grey;">${getDistanceTime(myblogs[i].postedAt)}</span>
+                    <span style="font-size: 15px; color: grey;">9 hour ago</span>
                 </div>
             </div>
         </div>  
         `
     }
 }
-
-function getFullTime(time) {
-    let date = time.getDate()
-    let monthIndex = time.getMonth()
-    let year = time.getFullYear()
-
-    let hour = time.getHours()
-    let minute = time.getMinutes()
-
-    return `${date} ${month[monthIndex]} ${year} ${hour}:${minute} WIB`
-}
-
-function getDistanceTime(time) {
-    let distance = new Date() - new Date(time)
-
-    let miliseconds = 1000
-    let secondInMinutes = 60
-    let minuteInHour = 60
-    let secondInHour = secondInMinutes * minuteInHour
-    let hourInDay = 23
-
-    let dayDistance = distance / (miliseconds * secondInHour * hourInDay)
-
-    if (dayDistance >= 1) {
-        const dayDate = Math.floor(dayDistance) + ' day ago'
-        return dayDate
-    } else {
-        let hourDistance = Math.floor(distance / (miliseconds * secondInHour))
-        if (hourDistance > 0) {
-            return hourDistance + ' hour ago'
-        } else {
-            let minuteDistance = Math.floor(distance / (miliseconds * secondInMinutes))
-            return minuteDistance + ' minute ago'
-        }
-    }
-
-}
-
-setInterval(function () {
-    rendermyblogs()
-}, 2000)
